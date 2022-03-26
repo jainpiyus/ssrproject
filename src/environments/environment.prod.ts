94c014476f94fe25e06ca880b0c4f5841f346386
Red Themes <33415914+jainpiyus@users.noreply.github.com>
2022-03-26 11:26:01 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,4 @@
 export const environment = {
-  firebase: {
-    projectId: 'tastio-contact-tracing',
-    appId: '1:903082049219:web:63075846eaec30decbec76',
-    databaseURL: 'https://tastio-contact-tracing.firebaseio.com',
-    storageBucket: 'tastio-contact-tracing.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyBZ8jrPGFjaogZGchY83JCjJpcgN1LSrfw',
-    authDomain: 'tastio-contact-tracing.firebaseapp.com',
-    messagingSenderId: '903082049219',
-    measurementId: 'G-DFNBDF7WKF',
-  },
+  
   production: true
 };
